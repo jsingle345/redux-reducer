636dd6a82f8270dfbdba27c5db86daf4dacf681e
jsingle345 <jsingle345@gmail.com>
2020-11-30 13:45:06 -0600

    commit tree 65e111701738abd67d28c608368f1a14d6c3fa49

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,14 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {
+    friends: []
+}, action){
+
+    switch(action.type){
+        case "ADD_FRIEND": 
+            let friend = action.friend
+            return {friends: [...state.friends, friend] }
+        case "REMOVE_FRIEND":
+               let newFriends = state.friends.filter(friend => friend.id !== action.id)
+            return {friends: newFriends}
+        default: return state
+    }
 }
